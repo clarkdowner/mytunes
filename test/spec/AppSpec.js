45b581a155d1bc92d0426b3592dc94de10175dc1
clarkdowner <cddowner@gmail.com>
2016-09-28 16:51:18 -0700

    Queue the next song when "enqueue" event is fired

--- test/spec/AppSpec.js
+++ test/spec/AppSpec.js
@@ -25,9 +25,10 @@ describe('App', function() {
     expect(app.get('currentSong')).to.be.an('object');
     app.get('library').at(0).play();
     expect(app.get('currentSong')).to.equal(app.get('library').at(0));
+    console.log(app);
   });
 
-  xit('queues the next song when an "enqueue" event is fired', function() {
+  it('queues the next song when an "enqueue" event is fired', function() {
     app.get('library').at(1).enqueue();
     expect(app.get('songQueue').at(0)).to.equal(app.get('library').at(1));
   });
