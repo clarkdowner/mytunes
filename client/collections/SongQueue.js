6071ef354826b51c93b62d92a4acaf6b1dd9e251
clarkdowner <cddowner@gmail.com>
2016-09-28 20:07:28 -0700

    Pass SongQueue tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,27 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function() {
+      this.shift();
+      if (this.length) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function() {
+      this.shift();
+    }, this);
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
 
 });
